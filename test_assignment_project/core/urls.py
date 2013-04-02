c94b8a1ca284273efbac17ea25303771bcbab5f0
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2013-04-02 16:40:05 +0300

    ticket:5 added edit_contacts url

--- test_assignment_project/core/urls.py
+++ test_assignment_project/core/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('test_assignment_project.core.views',
     url(r'^$', 'home', name='home'),
     url(r'^first-requests$', 'first_requests', name='first_requests'),
-    #url(r'^edit-contacts$', 'edit_contacts', name='edit_contacts'),
+    url(r'^edit-contacts$', 'edit_contacts', name='edit_contacts'),
 )
