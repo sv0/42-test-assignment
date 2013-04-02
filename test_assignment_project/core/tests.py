00792f60254cae977f7b4329ef04f154102f66f2
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2013-04-02 11:09:25 +0300

    ticket:1 core app tests

--- test_assignment_project/core/tests.py
+++ test_assignment_project/core/tests.py
@@ -1,16 +1,33 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.conf import LazySettings
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+from models import MyHttpRequest
+from forms import ProfileChangeForm
 
-Replace this with more appropriate tests for your application.
-"""
+TEST_USERNAME = 'fake'
+TEST_PASSWORD = 'fake'
+TEST_EMAIL = 'fake@example.com'
 
-from django.test import TestCase
+TEST_FORM_DATA = {
+    'first_name': 'fake',
+    'last_name': u'fake',
+    'date_of_birth': '1961-04-12',
+    'email': u'fake@example.com',
+}
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class ContactTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that home page contains required information.
         """
-        self.assertEqual(1 + 1, 2)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Name')
+        self.assertContains(response, 'Email')
+        self.assertContains(response, 'Skype')
+        self.assertContains(response, 'Bio')
