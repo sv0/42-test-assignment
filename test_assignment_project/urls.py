182d0acbabf193d39cc4bba6cd2b7cc113a9f141
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2013-04-02 11:55:23 +0300

    ticket:1 project's urls

--- test_assignment_project/urls.py
+++ test_assignment_project/urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf import settings
+from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'test_assignment_project.views.home', name='home'),
-    # url(r'^test_assignment_project/', include('test_assignment_project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'test_assignment_project.core.views.home', name='home'),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
